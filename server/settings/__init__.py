9cfc8b3d82c81ef267f206d920604c43824d0835
Cosmin Tupangiu <cosmin.tupangiu@gmail.com>
2019-02-20 08:40:21 +0100

    split the settings using djangi-split-settings

---
+++ server/settings/__init__.py
@@ -0,0 +1,27 @@
+from os import environ
+
+from split_settings.tools import optional, include
+
+
+# Managing environment via DJANGO_ENV variable:
+environ.setdefault('DJANGO_ENV', 'development')
+ENV = environ['DJANGO_ENV']
+
+base_settings = [
+    'components/common.py',
+    'components/logging.py',
+    'components/csp.py',
+    'components/db.py',
+
+    # You can even use glob:
+    # 'components/*.py'
+
+    # Select the right env:
+    'environments/{0}.py'.format(ENV),
+
+    # Optionally override some settings:
+    optional('environments/local.py'),
+]
+
+# Include settings:
+include(*base_settings)
--- server/wsgi.py
+++ server/wsgi.py
@@ -10,12 +10,10 @@ https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 import os
 import sys
 
-
-#sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../../")))
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../")))
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "HomeSite.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "server.settings")
 
 application = get_wsgi_application()
