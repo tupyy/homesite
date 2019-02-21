9a9be7c20ca566751e07f0eb974297023d81a817
Cosmin Tupangiu <cosmin.tupangiu@gmail.com>
2019-02-21 13:20:05 +0100

    add FilterPaymentViewMixin which filter the query set by category name

--- money/views/payments/controllers2.py
+++ money/views/payments/controllers2.py
@@ -2,10 +2,7 @@ import calendar
 from datetime import date
 
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.http import HttpResponseRedirect, JsonResponse
-from django.shortcuts import redirect
-from django.utils.http import is_safe_url
-from django.views.decorators.csrf import csrf_exempt
+from django.http import JsonResponse
 from django.views.generic import ListView, DetailView
 
 from money.models import Payment, Category
@@ -20,6 +17,14 @@ class MonthViewMixin(object):
         context['selected_month'] = self.get_selected_month()
         context['current_month'] = self.kwargs.get('month', None)
         return context
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        month_id = self.kwargs.get('month', None)
+        if not month_id:
+            return qs.filter(date__month=date.today().month, date__year=date.today().year)
+        else:
+            return qs.filter(date__month=month_id, date__year=date.today().year)
 
     def get_months_name(self):
         """
@@ -44,21 +49,24 @@ class CategoryViewMixin(object):
         return context
 
 
-class PaymentView(LoginRequiredMixin, MonthViewMixin, CategoryViewMixin, ListView):
+class FilterPaymentViewMixin(object):
+    model = Payment
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        query = self.request.GET.get('category', None)
+        if not query:
+            return qs
+        return qs.filter(category__name__exact=query)
+
+
+class PaymentView(FilterPaymentViewMixin, MonthViewMixin, CategoryViewMixin, LoginRequiredMixin, ListView):
     login_url = '/accounts/login/'
     redirect_field_name = 'redirect_to'
     model = Payment
     paginate_by = 10
     context_object_name = 'payments'
     template_name = 'money/payment/view_month.html'
-
-    def get_queryset(self):
-        qs = super().get_queryset()
-        month_id = self.kwargs.get('month', None)
-        if not month_id:
-            return qs.filter(date__month=date.today().month, date__year=date.today().year)
-        else:
-            return qs.filter(date__month=month_id, date__year=date.today().year)
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
