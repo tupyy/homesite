faa65a13f0aa16fc52960300546f1a08ad37a4f3
Cosmin Tupangiu <cosmin.tupangiu@gmail.com>
2019-02-22 09:10:48 +0100

    fix url

--- money/static/money/js/view_month.js
+++ money/static/money/js/view_month.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
     let month_select = document.getElementById('select_luna');
     month_select.addEventListener("change", function() {
        month_index = month_select.selectedIndex + 1;
-       window.location.href='/money/payment/view/'+ month_index + '/';
+       window.location.href='/money/payment/'+ month_index + '/';
     });
 
 });
