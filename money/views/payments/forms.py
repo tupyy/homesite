b5234a91d4f4ae918be28a7b25c05639a06c610b
Cosmin Tupangiu <cosmin.tupangiu@gmail.com>
2019-02-22 08:41:37 +0100

    remove tickete, contract from form

--- money/views/payments/forms.py
+++ money/views/payments/forms.py
@@ -1,22 +1,20 @@
+import datetime
+
 from django import forms
 from django.contrib.auth.models import User
 
 import server.settings
-from contract.models import Contract
-
 from money.models import Category, Subcategory, Payment
-import datetime
 
 
 class PaymentForm(forms.ModelForm):
     class Meta:
         model = Payment
-        fields = '__all__'
+        fields = ('sum',)
 
     next_url = forms.CharField(max_length=200)
     user = forms.ModelChoiceField(label="Name", queryset=User.objects.all(), empty_label=None)
     category = forms.ModelChoiceField(queryset=Category.objects.all(), empty_label=None)
-    contract = forms.ModelChoiceField(queryset=Contract.objects.filter(status__status__exact="Active"), required=False)
     subcategory = forms.ModelChoiceField(queryset=Subcategory.objects.all(), empty_label=None)
     date = forms.DateField(initial=datetime.date.today().strftime('%d/%m/%Y'),
                            input_formats=server.settings.DATE_INPUT_FORMATS)
