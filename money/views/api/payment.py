e0a3d969869cdb6d45fea1b473caa36ce83cf3db
Cosmin Tupangiu <cosmin.tupangiu@gmail.com>
2019-02-25 16:35:10 +0100

    fix get_query method get the initial qs by dates (either start and end date or year and month) next, filter the qs by cateogry subcategory if present

--- money/views/api/payment.py
+++ money/views/api/payment.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from django.shortcuts import get_object_or_404
 from rest_framework import generics, serializers
 from rest_framework import status
@@ -12,7 +14,7 @@ from money.serializer.payment import PaymentSerializer
 class PaymentViewSet(viewsets.ViewSetMixin, generics.ListAPIView):
     serializer_class = PaymentSerializer
 
-    permission_classes = (IsPostOrIsAuthenticated,)
+    # permission_classes = (IsPostOrIsAuthenticated,)
 
     def create(self, request):
         serializer = self.serializer_class(data=request.data)
@@ -28,40 +30,39 @@ class PaymentViewSet(viewsets.ViewSetMixin, generics.ListAPIView):
 
     def update(self, request, pk=None):
         payment = get_object_or_404(Payment.objects.all(), pk=pk)
-
         try:
             serializer = self.serializer_class(instance=payment, data=request.data)
             if serializer.is_valid():
-                serializer.save()
                 return Response(serializer.data, status=status.HTTP_200_OK)
             else:
                 return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-        except serializers.ValidationError as ex:
-            return Response(ex.detail, status=status.HTTP_400_BAD_REQUEST)
+        except (serializers.ValidationError, KeyError) as ex:
+            return Response(str(ex), status=status.HTTP_400_BAD_REQUEST)
 
     def get_queryset(self):
 
         start_date = self.request.query_params.get('start_date', None)
         end_date = self.request.query_params.get('end_date', None)
-        subcategorie = self.request.query_params.get('subcategory', None)
-        category = self.request.query_params.get('category', None)
-        month = self.request.query_params.get('month', None)
+        _subcategory = self.request.query_params.get('subcategory', None)
+        _category = self.request.query_params.get('category', None)
+        _month = self.request.query_params.get('month', None)
+        _year = self.request.query_params.get('year', None)
 
-        if start_date is not None and end_date is not None:
+        if start_date and end_date:
             queryset = Payment.objects.filter(date__gte=start_date, date__lte=end_date)
-        elif subcategorie is not None and category is not None and month is not None:
-            if subcategorie == "all":
-                queryset = Payment.objects.filter(category__name__exact=category,
-                                                  date__month=month)
-            else:
-                queryset = Payment.objects.filter(subcategory__name__exact=subcategorie,
-                                                  category__name__exact=category,
-                                                  date__month=month)
-        elif month is not None:
-            queryset = Payment.objects.filter(date__month=month)
         else:
-            return []
+            if _year:
+                queryset = Payment.objects.filter(date__year=_year)
+            else:
+                queryset = Payment.objects.filter(date__year=datetime.now().year)
+            if _month:
+                queryset = queryset.filter(date__month=_month)
 
+        if _subcategory and _category:
+            queryset = queryset.filter(category__name__exact=_category,
+                                       subcategory__name__exact=_subcategory,
+                                       date__month=_month)
+        elif _category and not _subcategory:
+            queryset = queryset.filter(category__name__exact=_category,
+                                       date__month=_month)
         return queryset
-
-
