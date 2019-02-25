aaed5d11b1fdff92cf3e881d1c0c01ea0ef632f5
Cosmin Tupangiu <cosmin.tupangiu@gmail.com>
2019-02-25 16:33:39 +0100

    remove try except block. it is useless

--- money/serializer/payment.py
+++ money/serializer/payment.py
@@ -13,50 +13,37 @@ class PaymentSerializer(serializers.Serializer):
     user = serializers.CharField(required=True)
     sum = serializers.DecimalField(max_digits=10, decimal_places=2, required=True)
     date = serializers.DateField(required=True)
-    comments = serializers.StringRelatedField()
+    comments = serializers.CharField(required=False)
 
     def create(self, validated_data):
-        try:
-            _category = get_object_or_404(Category.objects.all(), name=self.initial_data['category'])
-            _subcategory = Subcategory.objects.filter(name=self.initial_data['subcategory'],
+        if self.is_valid(raise_exception=True):
+            _category = Category.objects.get(name=self.validated_data['category'])
+            _subcategory = Subcategory.objects.filter(name=self.validated_data['subcategory'],
                                                       category__name=_category.name).first()
-            _user = get_object_or_404(User.objects.all(), username=self.initial_data['user'])
-            if self.is_valid():
-                _payment = Payment.objects.create(user=_user,
-                                                  category=_category,
-                                                  subcategory=_subcategory,
-                                                  sum=float(validated_data['sum']),
-                                                  date=validated_data['date'],
-                                                  comments=validated_data.get('comments', ''))
-                return _payment
-        except (ValueError, Http404) as ex:
-            raise serializers.ValidationError(str(ex))
-        except KeyError as err:
-            raise serializers.ValidationError('Field missing: ' + str(err))
+            _user = User.objects.get(username=self.validated_data['user'])
+            _payment = Payment.objects.create(user=_user,
+                                              category=_category,
+                                              subcategory=_subcategory,
+                                              sum=float(validated_data['sum']),
+                                              date=validated_data['date'],
+                                              comments=validated_data.get('comments', ''))
+            return _payment
 
     def update(self, instance, validated_data):
-        try:
-            _category = get_object_or_404(Category.objects.all(), name=self.initial_data['category'])
-            _subcategory = get_object_or_404(Subcategory.objects.all(), name=self.initial_data['subcategory'])
+        if self.is_valid(raise_exception=True):
+            _category = Category.objects.get(name=self.validated_data['category'])
+            _subcategory = Subcategory.objects.filter(name=self.validated_data['subcategory'],
+                                                      category__name=_category.name).first()
+            _user = User.objects.get(username=self.validated_data['user'])
 
-            if self.__is_valid_subcategory(_category.name, _subcategory.name):
-                user = get_object_or_404(User.objects.all(), username=self.initial_data['user'])
-
-                instance.category = _category
-                instance.user = user
-                instance.subcategory = _subcategory
-                instance.sum = validated_data.get("sum", instance.sum)
-                instance.date = validated_data.get('date', instance.date)
-                instance.comments = validated_data.get('comments', instance.comments)
-                instance.save()
-                return instance
-            else:
-                raise serializers.ValidationError(
-                    'Subcategory %s does not belong to category %s' % (_subcategory.name, _category.name))
-        except (ValueError, Http404) as ex:
-            raise serializers.ValidationError(str(ex))
-        except KeyError as err:
-            raise serializers.ValidationError('Field missing: ' + str(err))
+            instance.category = _category
+            instance.user = _user
+            instance.subcategory = _subcategory
+            instance.sum = validated_data.get("sum", instance.sum)
+            instance.date = validated_data.get('date', instance.date)
+            instance.comments = validated_data.get('comments', instance.comments)
+            instance.save()
+            return instance
 
     def validate_category(self, value):
         try:
@@ -71,8 +58,16 @@ class PaymentSerializer(serializers.Serializer):
         _subcategory = Subcategory.objects.filter(name=value,
                                                   category__name__exact=self.initial_data['category']).first()
         if not _subcategory:
-            raise ValidationError('Subcategory {} is not part of category {}'.format(value, self.initial_data['category']))
+            raise ValidationError(
+                'Subcategory {} is not part of category {}'.format(value, self.initial_data['category']))
         return value
+
+    def validate_user(self, value):
+        try:
+            _user = get_object_or_404(User, username=self.initial_data['user'])
+            return value
+        except Http404:
+            raise ValidationError('User {} do not exists.'.format(self.initial_data['user']))
 
 
 class TotalSerializer(serializers.Serializer):
