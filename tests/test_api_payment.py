55eeccbcaa452ed2af003ff917859ce54f29f7a8
Cosmin Tupangiu <cosmin.tupangiu@gmail.com>
2019-02-26 10:01:17 +0100

    add test for update

--- tests/test_api_payment.py
+++ tests/test_api_payment.py
@@ -34,6 +34,37 @@ class PaymentAPITestCase(TestCase):
         response = self.client.post('http://localhost:8000/api/money/payment/', _payment)
         self.assertEqual(response.status_code, 201)
 
+    def test_update_payment_nominal(self):
+        _payment = {"user": "admin",
+                    "category": "alimente",
+                    "subcategory": "mancare",
+                    "sum": 20,
+                    "date": "2019-02-03",
+                    "comments": "teste"
+                    }
+        response = self.client.post('http://localhost:8000/api/money/payment/', _payment)
+        self.assertEqual(response.status_code, 201)
+
+        _payment['sum'] = 40
+        response = self.client.put('http://localhost:8000/api/money/payment/{}/'.format(response.data['id']), _payment)
+        self.assertEqual(response.status_code, 200)
+
+    def test_update_payment_400(self):
+        _payment = {"user": "admin",
+                    "category": "alimente",
+                    "subcategory": "mancare",
+                    "sum": 20,
+                    "date": "2019-02-03",
+                    "comments": "teste"
+                    }
+        response = self.client.post('http://localhost:8000/api/money/payment/', _payment)
+        self.assertEqual(response.status_code, 201)
+
+        _payment['sum'] = 40
+        _payment['subcategory'] = 'bar'
+        response = self.client.put('http://localhost:8000/api/money/payment/{}/'.format(response.data['id']), _payment)
+        self.assertEqual(response.status_code, 400)
+
     def test_create_payment_no_category(self):
         _payment = {"user": "admin",
                     "category": "dont_exists",
