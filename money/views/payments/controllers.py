13f2e23c79335f98f6aed4aeda014feee475a2e4
Cosmin Tupangiu <cosmin.tupangiu@gmail.com>
2019-02-26 14:05:35 +0100

    set paginate to 15 items

--- money/views/payments/controllers.py
+++ money/views/payments/controllers.py
@@ -71,7 +71,7 @@ class PaymentsIndexView(MonthViewMixin, CategoryViewMixin, LoginRequiredMixin, L
     login_url = '/accounts/login/'
     redirect_field_name = 'redirect_to'
     model = Payment
-    paginate_by = 20
+    paginate_by = 15
     context_object_name = 'payments'
     template_name = 'money/payment/view_month.html'
 
