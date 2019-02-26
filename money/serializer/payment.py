b9192788e62611a952d8d76d134096a0c54b61e3
Cosmin Tupangiu <cosmin.tupangiu@gmail.com>
2019-02-26 09:35:53 +0100

    remove validation check on serilizer class. useless

--- money/serializer/payment.py
+++ money/serializer/payment.py
@@ -16,18 +16,17 @@ class PaymentSerializer(serializers.Serializer):
     comments = serializers.CharField(required=False)
 
     def create(self, validated_data):
-        if self.is_valid(raise_exception=True):
-            _category = Category.objects.get(name=self.validated_data['category'])
-            _subcategory = Subcategory.objects.filter(name=self.validated_data['subcategory'],
-                                                      category__name=_category.name).first()
-            _user = User.objects.get(username=self.validated_data['user'])
-            _payment = Payment.objects.create(user=_user,
-                                              category=_category,
-                                              subcategory=_subcategory,
-                                              sum=float(validated_data['sum']),
-                                              date=validated_data['date'],
-                                              comments=validated_data.get('comments', ''))
-            return _payment
+        _category = Category.objects.get(name=self.validated_data['category'])
+        _subcategory = Subcategory.objects.filter(name=self.validated_data['subcategory'],
+                                                  category__name=_category.name).first()
+        _user = User.objects.get(username=self.validated_data['user'])
+        _payment = Payment.objects.create(user=_user,
+                                          category=_category,
+                                          subcategory=_subcategory,
+                                          sum=float(validated_data['sum']),
+                                          date=validated_data['date'],
+                                          comments=validated_data.get('comments', ''))
+        return _payment
 
     def update(self, instance, validated_data):
         if self.is_valid(raise_exception=True):
@@ -45,29 +44,33 @@ class PaymentSerializer(serializers.Serializer):
             instance.save()
             return instance
 
-    def validate_category(self, value):
-        try:
-            _category = get_object_or_404(Category, name=self.initial_data['category'])
-            return value
-        except Http404:
-            raise ValidationError('Category {} do not exists.'.format(self.initial_data['category']))
+    def validate(self, data):
+        if 'category' not in data:
+            raise ValidationError('Category is missing.')
 
-    def validate_subcategory(self, value):
-        if not value:
-            raise ValidationError('Subcategory not present')
-        _subcategory = Subcategory.objects.filter(name=value,
-                                                  category__name__exact=self.initial_data['category']).first()
+        if 'subcategory' not in data:
+            raise ValidationError('Subcategory is missing.')
+
+        _subcategory = Subcategory.objects.filter(name=data.get('subcategory'),
+                                                  category__name__exact=data.get('category')).first()
         if not _subcategory:
             raise ValidationError(
-                'Subcategory {} is not part of category {}'.format(value, self.initial_data['category']))
-        return value
+                'Subcategory {} is not part of category {}'.format(data.get('subcategory'), data.get('category')))
+        return data
+
+    def validate_category(self, value):
+        try:
+            _category = get_object_or_404(Category, name=value)
+            return value
+        except Http404:
+            raise ValidationError('Category {} do not exists.'.format(value))
 
     def validate_user(self, value):
         try:
-            _user = get_object_or_404(User, username=self.initial_data['user'])
+            _user = get_object_or_404(User, username=value)
             return value
         except Http404:
-            raise ValidationError('User {} do not exists.'.format(self.initial_data['user']))
+            raise ValidationError('User {} do not exists.'.format(value))
 
 
 class TotalSerializer(serializers.Serializer):
