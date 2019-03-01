c2c7378c9404a4cb396149cf8924f673e128fc10
Cosmin Tupangiu <cosmin.tupangiu@gmail.com>
2019-03-01 09:33:10 +0100

    remove db prod

--- server/settings/components/db.py
+++ server/settings/components/db.py
@@ -8,14 +8,3 @@ DATABASES_DEV = {
         'PORT': '5432',
     }
 }
-
-DATABASES_PROD = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'd87nfbqvf8bdv9',
-        'USER': 'xvzammcverouba',
-        'PASSWORD': 'e194015546093749863526367a2ab3a3b9827c2e66fa94092025a0d958d4430d',
-        'HOST': 'ec2-54-217-205-90.eu-west-1.compute.amazonaws.com',
-        'PORT': '5432',
-    }
-}
